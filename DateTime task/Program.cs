7e302cd1b55e3cdb908a30f14dc58e283a1c62a9
Ali <lisalmanov84@gmail.com>
2023-07-06 16:19:44 +0400

    Date Time

---
+++ DateTime task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DateTime_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a date (dd_MM_yyyy): ");
+            string dateTime = Console.ReadLine();
+
+            try
+            {
+                DateTime date = DateTime.ParseExact(dateTime, "dd_MM_yyyy", null);
+                Console.WriteLine("Tarix: " + date.ToString("dd/MM/yyyy"));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Wrong date " + ex.Message);
+            }
+        }
+    }
+}
